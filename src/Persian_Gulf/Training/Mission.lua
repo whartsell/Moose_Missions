539ceb56ba20f70840c67522cb5e9cc28e7604ba
whartsell <will@willshouse.net>
2018-07-01 00:13:24 -0700

    added second CSG and cap with teeth

--- src/Persian_Gulf/Training/Mission.lua
+++ src/Persian_Gulf/Training/Mission.lua
@@ -29,15 +29,23 @@ Red_A2ADDispatcher:SetEngageRadius(100000) -- 100km engagement zone
 --Squadrons
 Red_A2ADDispatcher:SetSquadron("Lar Drones","Lar Airbase",{"Red Drone MiG-29","Red Drone F4","Red Drone F-14","Red Drone F-5"})
 Red_A2ADDispatcher:SetSquadronOverhead("Lar Drones",1)
-Red_A2ADDispatcher:SetSquadronGrouping("Lar Drones",4)
+Red_A2ADDispatcher:SetSquadronGrouping("Lar Drones",2)
 Red_A2ADDispatcher:SetSquadronTakeoffFromRunway("Lar Drones")
 Red_A2ADDispatcher:SetSquadronLandingAtRunway("Lar Drones")
+--
+Red_A2ADDispatcher:SetSquadron("Bandar AbbasSQ","Bandar Abbas Intl",{"Red Squadron Bandar Abbas"},12)
+Red_A2ADDispatcher:SetSquadronOverhead("Bandar AbbasSQ",1)
+Red_A2ADDispatcher:SetSquadronGrouping("Bandar AbbasSQ",2)
+Red_A2ADDispatcher:SetSquadronTakeoffFromRunway("Bandar AbbasSQ")
+Red_A2ADDispatcher:SetSquadronLandingAtRunway("Bandar AbbasSQ")
 -- without a cap zone and CAP defined (below) the squadron will do nothing
 -- CAP zones
 -- example
 -- Bamdar Abbas CAP is name of the helo group defining the cap zone.  it must be late activated
+CAPZoneBA = ZONE_POLYGON:NewFromGroupName("BandarAbbasCAP",GROUP:FindByName("BandarAbbasCAP"))
 CAPZoneDrones = ZONE_POLYGON:NewFromGroupName("DroneCAP",GROUP:FindByName("DroneCAP"))
 Red_A2ADDispatcher:SetSquadronCap("Lar Drones",CAPZoneDrones,5000,5000,750,950,950,1800,"BARO")
-Red_A2ADDispatcher:SetSquadronCapInterval("Lar Drones",1,30,30,1)
-
+Red_A2ADDispatcher:SetSquadronCapInterval("Lar Drones",2,30,30,1)
+Red_A2ADDispatcher:SetSquadronCap("Bandar AbbasSQ",CAPZoneBA,7620,7620,750,950,950,1800,"BARO")
+Red_A2ADDispatcher:SetSquadronCapInterval("Bandar AbbasSQ",1,30,30,1)
 
