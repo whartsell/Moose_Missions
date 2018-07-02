f691dacce7a421da0d6b272a6f07aaec3124028f
whartsell <will@willshouse.net>
2018-07-01 22:01:10 -0700

    mission completed and ready for test

--- src/Persian_Gulf/Campaign/Mission01/Mission01.lua
+++ src/Persian_Gulf/Campaign/Mission01/Mission01.lua
@@ -12,7 +12,7 @@ Detection = DETECTION_AREAS:New(DetectionSetGroup,30000)
 Red_A2ADDispatcher = AI_A2A_DISPATCHER:New(Detection)
 
 -- enable the tactical display for debugging
-Red_A2ADDispatcher:SetTacticalDisplay( false )
+Red_A2ADDispatcher:SetTacticalDisplay( true )
 
 -- Iran's border zone
 Red_BorderZone = ZONE_POLYGON:NewFromGroupName("Red Border",GROUP:FindByName("Red Border"))
@@ -26,18 +26,19 @@ Red_A2ADDispatcher:SetBorderZone(Red_BorderZone)
 -- Engagement
 Red_A2ADDispatcher:SetEngageRadius(100000) -- 100km engagement zone
 
+
 --Squadrons
-Red_A2ADDispatcher:SetSquadron("Lar Drones","Lar Airbase",{"Red Drone MiG-29","Red Drone F4","Red Drone F-14","Red Drone F-5"})
-Red_A2ADDispatcher:SetSquadronOverhead("Lar Drones",1)
-Red_A2ADDispatcher:SetSquadronGrouping("Lar Drones",4)
-Red_A2ADDispatcher:SetSquadronTakeoffFromRunway("Lar Drones")
-Red_A2ADDispatcher:SetSquadronLandingAtRunway("Lar Drones")
--- without a cap zone and CAP defined (below) the squadron will do nothing
+Red_A2ADDispatcher:SetSquadron("Bandar AbbasSQ","Bandar Abbas Intl",{"Red Squadron Bandar Abbas"},8)
+Red_A2ADDispatcher:SetSquadronGci("Bandar AbbasSQ",1000,1800)
+Red_A2ADDispatcher:SetSquadronOverhead("Bandar AbbasSQ",1)
+Red_A2ADDispatcher:SetSquadronGrouping("Bandar AbbasSQ",2)
+Red_A2ADDispatcher:SetSquadronTakeoffFromRunway("Bandar AbbasSQ")
+Red_A2ADDispatcher:SetSquadronLandingAtRunway("Bandar AbbasSQ")
+
 -- CAP zones
--- example
--- Bamdar Abbas CAP is name of the helo group defining the cap zone.  it must be late activated
-CAPZoneDrones = ZONE_POLYGON:NewFromGroupName("DroneCAP",GROUP:FindByName("DroneCAP"))
-Red_A2ADDispatcher:SetSquadronCap("Lar Drones",CAPZoneDrones,5000,5000,750,950,950,1800,"BARO")
-Red_A2ADDispatcher:SetSquadronCapInterval("Lar Drones",1,30,30,1)
+CAPZoneSouth = ZONE_POLYGON:NewFromGroupName("Bandar Abbas CAP",GROUP:FindByName("Bandar Abbas CAP"))
+Red_A2ADDispatcher:SetSquadronCap("Bandar AbbasSQ",CAPZoneSouth,7620,7620,750,950,950,1800,"BARO")
+Red_A2ADDispatcher:SetSquadronCapInterval("Bandar AbbasSQ",1,300,600,1)
+
 
 
