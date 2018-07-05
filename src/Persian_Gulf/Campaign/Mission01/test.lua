bbd430e0292e72503c9bddd5d1609a9b5a9f6334
whartsell <will@willshouse.net>
2018-07-05 11:19:39 -0700

    V66 of training mission
    On demand tankers availible at each carrier group in their respective menus.
    S3s will spawn on cat (or wait till one available), take off fly orbit providing fuel till bingo then land and taxi to parking.
    
    TODO: need to add a "cleanup" item to handle cases where the tanker doesnt do what we want

---
+++ src/Persian_Gulf/Campaign/Mission01/test.lua
@@ -0,0 +1,12 @@
+CC = COMMANDCENTER:New(GROUP:FindByName("FFG-58"),"FFG-58")
+
+NavalSetGroup = SET_GROUP:New():FilterPrefixes( "FFG-58" ):FilterStart()
+NavalDetection = DETECTION_UNITS:New( NavalSetGroup )
+NavalDetection:SetRefreshTimeInterval(30)
+NavalDetection:Start()
+function NavalDetection:OnAfterDetect(From,Event,To)
+
+  local DetectionReport = NavalDetection:DetectedReportDetailed()
+  --local DetectionReport = NavalDetection:DetectedItemReportSummary()
+  CC:GetPositionable():MessageToAll( DetectionReport, 15, "" )
+end
--- src/Persian_Gulf/Training/Mission.lua
+++ src/Persian_Gulf/Training/Mission.lua
@@ -50,10 +50,41 @@ Red_A2ADDispatcher:SetSquadronCap("Bandar AbbasSQ",CAPZoneBA,7620,7620,750,950,9
 Red_A2ADDispatcher:SetSquadronCapInterval("Bandar AbbasSQ",1,30,30,1)
 
 --Tankers
-Spawn_Shell = SPAWN:New("Shell"):InitLimit(1,10)
-Spawn_Shell:InitRepeatOnEngineShutDown()
-Spawn_Shell:SpawnScheduled(30,0)
+--local Spawn_Shell = SPAWN:New("Shell"):InitLimit(1,10)
+--Spawn_Shell:InitRepeatOnEngineShutDown()
+--Spawn_Shell:SpawnScheduled(30,0)
+local Spawn_Arco = SPAWN:New("Arco"):InitLimit(1,10)
+Spawn_Arco:InitRepeatOnEngineShutDown()
+Spawn_Arco:SpawnScheduled(30,0)
+local Spawn_Texaco = SPAWN:New("Texaco"):InitLimit(1,10)
+Spawn_Texaco:InitRepeatOnEngineShutDown()
+Spawn_Texaco:SpawnScheduled(30,0)
 
+--Missile Trainer
+local Trainer = MISSILETRAINER
+  :New( 300, "Missile Training Script is active" )
+  :InitMessagesOnOff(true)
+  :InitAlertsToAll(true) 
+  :InitAlertsHitsOnOff(true)
+  :InitAlertsLaunchesOnOff(false)
+  :InitBearingOnOff(true)
+  :InitRangeOnOff(true)
+  :InitTrackingOnOff(true)
+  :InitTrackingToAll(true)
+  :InitMenusOnOff(true)
+
+Trainer:InitAlertsToAll(true)
+ 
+ 
+ 
+function launchTanker(SpawnTanker)
+  local Spawn_S3 = SPAWN:New(SpawnTanker):InitLimit(1,1)
+  Spawn_S3:InitRepeatOnLanding()
+  Spawn_S3:Spawn()
+end
 
 --Menu
-local MenuCoalitionBlue = MENU_COALITION:New(coalition.side.BLUE,"Manage Menus")
+local Menu_CSG1 = MENU_COALITION:New(coalition.side.BLUE,"CSG-1")
+local Menu_CSG2 = MENU_COALITION:New(coalition.side.BLUE,"CSG-2")
+MENU_COALITION_COMMAND:New(coalition.side.BLUE,"Launch Shell",Menu_CSG1,launchTanker,"Shell")
+MENU_COALITION_COMMAND:New(coalition.side.BLUE,"Launch Arco",Menu_CSG2,launchTanker,"Arco")
