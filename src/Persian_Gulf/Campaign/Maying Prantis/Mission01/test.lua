8b36277759603c1b05db822497160574a3ec76cf
whartsell <will@willshouse.net>
2018-07-05 23:15:51 -0700

    latest changes
    added voiceovers..renamed dirs

--- src/Persian_Gulf/Campaign/Maying Prantis/Mission01/test.lua
+++ src/Persian_Gulf/Campaign/Maying Prantis/Mission01/test.lua
@@ -1,7 +1,8 @@
 CC = COMMANDCENTER:New(GROUP:FindByName("FFG-58"),"FFG-58")
 
 NavalSetGroup = SET_GROUP:New():FilterPrefixes( "FFG-58" ):FilterStart()
-NavalDetection = DETECTION_UNITS:New( NavalSetGroup )
+--NavalDetection = DETECTION_UNITS:New( NavalSetGroup )
+NavalDetection = DETECTION_AREAS:New(NavalSetGroup,2000)
 NavalDetection:SetRefreshTimeInterval(30)
 NavalDetection:Start()
 function NavalDetection:OnAfterDetect(From,Event,To)
